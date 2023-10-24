6c23bc4be378d65eca9041a19427e7f4236023e9
gremwiz1 <gremwiz@yandex.ru>
2023-10-25 00:31:23 +0300

    Updates

--- src/services/actions/actions.ts
+++
@@ -1,6 +0,0 @@
-export const SAVE_VIDEO_TIME = "SAVE_VIDEO_TIME";
-
-export const saveVideoTime = (time: number) => ({
-  type: SAVE_VIDEO_TIME,
-  payload: time,
-});
--- src/utils/api/api-validate-telephone.ts
+++
@@ -1,9 +0,0 @@
-export const validateTelephoneNumber = (telephone: string) => {
-  return fetch(`http://apilayer.net/api/validate?access_key=40f9d88b9f643c2271948f1d9db131c2&number=
-    ${telephone}&country_code=RU&format=1`).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Ошибка: ${res.status}`);
-  });
-};
